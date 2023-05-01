bedc98c8fd683744f09b1cbdf44055d5c8978cb8
Danny <perez6736@gmail.com>
2023-05-01 17:42:21 +0900

    moved on click to only buttons

--- src/componenets/App.js
+++ src/componenets/App.js
@@ -20,30 +20,26 @@ function App() {
           className={`bg-white rounded-xl shadow-lg p-8 flex flex-col items-center justify-around h-1/2 w-1/4 ${
             showBack ? "animate-flip" : ""
           }`}
-          onClick={handleClick}
         >
           {showBack ? (
             <div className="flex flex-col items-center">
               <h1 className="text-6xl font-bold">B</h1>
               <div className="flex justify-center w-full mt-8">
-                <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-4 px-8 rounded-lg mr-4">
+                <Button success rounded className="mr-4" onClick={handleClick}>
                   Correct
-                </button>
-                <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-4 px-8 rounded-lg">
+                </Button>
+                <Button danger rounded onClick={handleClick}>
                   Incorrect
-                </button>
+                </Button>
               </div>
             </div>
           ) : (
             <div className="flex flex-col items-center">
               <h1 className="text-6xl font-bold">A</h1>
               <div className="flex justify-center w-full mt-8">
-                <button
-                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-8 rounded-lg"
-                  onClick={handleClick}
-                >
+                <Button primary onClick={handleClick}>
                   Show Answer
-                </button>
+                </Button>
               </div>
             </div>
           )}
